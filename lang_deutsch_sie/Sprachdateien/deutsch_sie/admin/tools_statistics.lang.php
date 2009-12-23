93e2d38fedf2eb8c7103a99fd30917b6e4e7eb35
Stefan <mybbcms@yahoo.de>
2009-12-23 12:04:24 +0000

    Update auf rev4643
    git-svn-id: http://svn.mybboard.de/trunk@151 89cb5571-0ea1-49a6-b9ba-2e11e5d2b012

---
+++ lang_deutsch_sie/Sprachdateien/deutsch_sie/admin/tools_statistics.lang.php
@@ -0,0 +1,34 @@
+<?php
+#########################################################
+# Deutsches Sprachpaket (Formell)                       #
+# Version x.x.x                                         #
+# Datum: xx.xx.xxxx                                     #
+# MyBB-Version x.x.x                                    #
+# Autor: MyBBoard.de | Webseite: http://www.mybboard.de #
+# (c) 2005-2009 MyBBoard.de | Alle Rechte vorbehalten!  #
+#                                                       #
+# Die Lizenz/Nutzungsbedingungen finden Sie in der      #
+# beiliegenden Readme.                                  #
+#########################################################
+
+$l['statistics'] = "Statistiken";
+$l['overall_statistics'] = "Gesamte Statistiken";
+$l['overall_statistics_desc'] = "Hier können Sie Statistiken zu ihrem Forum ansehen.";
+
+$l['date_range'] = "Datumbereich";
+
+$l['date'] = "Datum";
+$l['users'] = "Benutzer";
+$l['threads'] = "Themen";
+$l['posts'] = "Beiträge";
+
+$l['from'] = "Von";
+$l['to'] = "bis";
+
+$l['increase'] = "Steigend";
+$l['no_change'] = "Keine Veränderung";
+$l['decrease'] = "Fallend";
+
+$l['error_no_results_found_for_criteria'] = "Es konnten leider keine Ergebnisse für den Datumsbreich gefunden werden. Bitte wählen Sie einen anderen Bereich.";
+$l['error_no_statistics_available_yet'] = "Es tut uns Leid, aber es gibt keine Daten für die Statistiken in ihrem Forum.";
+?>
