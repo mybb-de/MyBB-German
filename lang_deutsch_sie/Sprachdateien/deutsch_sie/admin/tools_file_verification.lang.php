d103ae3fbdb7fb60d0c81e2339dabee5a3c1c4ad
Stefan <mybbcms@yahoo.de>
2009-12-31 16:09:17 +0000

    Fehlende Datei hinzugefügt
    git-svn-id: http://svn.mybboard.de/trunk@154 89cb5571-0ea1-49a6-b9ba-2e11e5d2b012

---
+++ lang_deutsch_sie/Sprachdateien/deutsch_sie/admin/tools_file_verification.lang.php
@@ -0,0 +1,26 @@
+<?php
+#########################################################
+# Deutsches Sprachpaket (Formell)                       #
+# Version x.x.x                                         #
+# Datum: xx.xx.xxxx                                     #
+# MyBB-Version x.x.x                                    #
+# Autor: MyBBoard.de | Webseite: http://www.mybboard.de #
+# (c) 2005-2009 MyBBoard.de | Alle Rechte vorbehalten!  #
+#                                                       #
+# Die Lizenz/Nutzungsbedingungen finden Sie in der      #
+# beiliegenden Readme.                                  #
+#########################################################
+
+$l['file_verification'] = "Datei-Überprüfung";
+$l['checking'] = "Überprüfen...";
+$l['file_verification_message'] = "Dies wird die Dateien des MyBBs auf ihre Gültigkeit überprüfen. Drücken Sie Ja um fortzufahren.<br /> <small>Bitte beachten Sie, dass kein Unterschied zwischen gewollten Veränderungen und Beschädigungen gemacht wird. Deshalb sollten Sie beim Zurücksetzen von \"beschädigten\" Dateien vorsichtig sein.</small>";
+
+$l['error_communication'] = "Es gab ein Problem bei der Kommunikation mit dem MyBB-Server. Bitte versuchen Sie es in ein paar Minuten noch einmal.";
+$l['file'] = "Datei";
+$l['no_corrupt_files_found'] = "Gratulation! Es konnten keine beschädigten Dateien gefunden werden.";
+$l['found_problems'] = "Gefundene Probleme";
+$l['changed'] = "Verändert";
+$l['missing'] = "Fehlend";
+$l['status'] = "Status";
+
+?>
