a644dec5d9841880dee67cb124112b287c7a5c00
Stefan-ST <mybbcms@yahoo.de>
2013-12-23 11:46:01 +0100

    MyBB 1.6.12

--- lang_deutsch_sie/Sprachdateien/deutsch_sie/datahandler_post.lang.php
+++ lang_deutsch_sie/Sprachdateien/deutsch_sie/datahandler_post.lang.php
@@ -22,6 +22,7 @@ $l['postdata_post_flooding'] = 'Sie versuchen, eine Nachricht zu kurz nach der v
 $l['postdata_post_flooding_one_second'] = 'Sie versuchen, eine Nachricht zu kurz nach der vorigen zu schreiben. Bitte warten Sie noch 1 Sekunde.';
 $l['postdata_too_many_images'] = 'Die von Ihnen eingegebene Nachricht enthält {1} Grafiken, es sind aber nur {2} Grafiken pro Nachricht erlaubt. Bitte entfernen Sie Grafiken aus Ihrer Nachricht, um das Limit nicht zu überschreiten.';
 $l['postdata_too_many_videos'] = 'Die von Ihnen eingegebene Nachricht enthält {1} Videos, es sind aber nur {2} Videos pro Nachricht erlaubt. Bitte entfernen Sie Videos aus Ihrer Nachricht, um das Limit nicht zu überschreiten.';
+$l['postdata_invalid_prefix'] = 'Das ausgewählte Präfix ist ungültig. Bitte wählen Sie ein gültiges Präfix aus.';
 
 $l['thread_closed'] = "Thema geschlossen";
 $l['thread_opened'] = "Thema geöffnet";
