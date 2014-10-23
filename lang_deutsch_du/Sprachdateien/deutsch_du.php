0451d35e6720973bae32152e739a8ff00cea56b7
Stefan-ST <mybbcms@yahoo.de>
2014-10-23 20:13:08 +0200

    MyBB 1.8.1

--- lang_deutsch_du/Sprachdateien/deutsch_du.php
+++ lang_deutsch_du/Sprachdateien/deutsch_du.php
@@ -18,7 +18,7 @@ $langinfo['author'] = "MyBBoard.de";
 $langinfo['website'] = "http://www.mybboard.de/";
 
 // Compatible version of MyBB
-$langinfo['version'] = "1800";
+$langinfo['version'] = "1801";
 
 // Sets if the translation includes the Admin CP (1 = yes, 0 = no)
 $langinfo['admin'] = 1;
