c68ca658bbd3dfe0877b46d5a7a5755e0b3ff1af
Stefan-ST <544140+Stefan-MyBB@users.noreply.github.com>
2015-01-22 16:38:36 +0100

    Benutzer-CP Home

--- lang_deutsch_du/Sprachdateien/deutsch_du/usercpnav.lang.php
+++ lang_deutsch_du/Sprachdateien/deutsch_du/usercpnav.lang.php
@@ -31,6 +31,6 @@ $l['ucp_nav_drafts'] = "Entwürfe";
 $l['ucp_nav_drafts_active'] = "<strong>Entwürfe ({1})</strong>";
 $l['ucp_nav_notepad'] = "Notizbuch";
 $l['ucp_nav_view_profile'] = "Profil ansehen";
-$l['ucp_nav_home'] = "Benutzer-CP Home";
+$l['ucp_nav_home'] = "Benutzer-CP Startseite";
 $l['ucp_nav_usergroups'] = "Gruppenmitgliedschaften";
 $l['ucp_nav_attachments'] = "Attachments";
