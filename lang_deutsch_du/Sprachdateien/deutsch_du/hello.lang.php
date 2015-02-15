4682c92e828fb75a6eb7f175dde47b6c70884be3
Stefan-ST <544140+Stefan-MyBB@users.noreply.github.com>
2015-02-15 22:52:21 +0100

    MyBB 1.8.4

---
+++ lang_deutsch_du/Sprachdateien/deutsch_du/hello.lang.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * German language pack for MyBB 1.8 (informal)
+ * Deutsches Sprachpaket für MyBB 1.8 "informell" (Du)
+ * (c) 2005-2014 MyBBoard.de
+ * 
+ * Author/Autor: http://www.mybboard.de/
+ * License/Lizenz: GNU Lesser General Public License, Version 3
+ */
+
+$l['hello'] = "Hallo Welt!";
+$l['hello_add'] = "Hinzufügen";
+$l['hello_add_message'] = "Nachricht hinzufügen";
+$l['hello_empty'] = "Es wurden keine Nachrichten gefunden.";
+$l['hello_message_empty'] = "Die Nachricht darf nicht leer sein.";
+$l['hello_done'] = "Neue Nachricht erfolgreich hinzugefügt.";
---
+++ lang_deutsch_sie/Sprachdateien/deutsch_sie/admin/hello.lang.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * German language pack for MyBB 1.8 (formal)
+ * Deutsches Sprachpaket für MyBB 1.8 "formell" (Sie)
+ * (c) 2005-2014 MyBBoard.de
+ * 
+ * Author/Autor: http://www.mybboard.de/
+ * License/Lizenz: GNU Lesser General Public License, Version 3
+ */
+
+$l['hello_desc'] = "Ein Beispiel-Plugin, das es Ihnen erlaubt Nachrichten auf der Startseite zu erstellen und an jeden Beitrag anzuhängen.";
+
+$l['setting_group_hello'] = "Hallo Welt!";
+$l['setting_group_hello_desc'] = "Einstellungen für das Hallo Welt! Plugin";
+
+$l['setting_hello_display1'] = "Nachrichten auf der Startseite anzeigen";
+$l['setting_hello_display1_desc'] = "Wählen Sie Nein, wenn keine Nachrichten auf der Startseite angezeigt werden sollen.";
+
+$l['setting_hello_display2'] = "Nachrichten in Beiträgen anzeigen";
+$l['setting_hello_display2_desc'] = "Wählen Sie Nein, wenn keine Nachrichten in Nachrichten angezeigt werden sollen.";
+
+$l['hello_uninstall'] = "Hallo Welt! Deinstallation";
+$l['hello_uninstall_message'] = "Sollen alle Nachrichten aus der Datenbank gelöscht werden?";
