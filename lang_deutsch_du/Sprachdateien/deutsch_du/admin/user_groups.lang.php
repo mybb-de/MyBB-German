d14a7f28a4d08fc1a4e16dbfbc872df28816fa84
Stefan-ST <mybbcms@yahoo.de>
2015-04-15 16:46:54 +0200

    Fix: https://www.mybb.de/forum/thread-31160-post-211681.html#pid211681

--- lang_deutsch_du/Sprachdateien/deutsch_du/admin/user_groups.lang.php
+++ lang_deutsch_du/Sprachdateien/deutsch_du/admin/user_groups.lang.php
@@ -125,7 +125,7 @@ $l['can_delete_threads'] = "Kann eigene Themen löschen";
 $l['can_edit_attachments'] = "Kann eigene Attachments aktualisieren";
 $l['account_management'] = "Berechtigungen: Accounts verwalten";
 $l['edit_time_limit'] = "Bearbeitungs-Limit";
-$l['edit_time_limit_desc'] = "Der Zeitraum im Minuten, bevor Benutzer ihre Beiträge nicht mehr bearbeiten können, falls sie die nötigen Rechte haben (0 = unendlich).";
+$l['edit_time_limit_desc'] = "Der Zeitraum in Minuten, bevor Benutzer ihre Beiträge nicht mehr bearbeiten können, falls sie die nötigen Rechte haben (0 = unendlich).";
 $l['can_be_reported'] = "Kann gemeldet werden?";
 $l['can_access_usercp'] = "Kann Benutzer-CP betreten";
 $l['can_change_username'] = "Kann Benutzernamen wechseln";
