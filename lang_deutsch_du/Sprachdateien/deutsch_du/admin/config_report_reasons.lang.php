d39e5fa70a1ed8cd873a0ebb00fc604532f52051
Stefan-ST <544140+Stefan-MyBB@users.noreply.github.com>
2016-10-17 14:05:57 +0200

    MyBB 1.8.8

---
+++ lang_deutsch_du/Sprachdateien/deutsch_du/admin/config_report_reasons.lang.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * German language pack for MyBB 1.8 (informal)
+ * Deutsches Sprachpaket für MyBB 1.8 "informell" (Du)
+ * (c) 2005-2014 MyBBoard.de
+ * 
+ * Author/Autor: http://www.mybboard.de/
+ * License/Lizenz: GNU Lesser General Public License, Version 3
+ */
+
+$l['report_reasons'] = "Melde-Gründe";
+$l['report_reasons_desc'] = "Hier kannst du Gründe erstellen und verwalten, die Benutzer beim Melden von Inhalten auswählen können.";
+$l['add_new_reason'] = "Neuen Grund hinzufügen";
+$l['add_new_reason_desc'] = "Füge einen neuen Grund hinzu, den Bentzer beim Melden von Inhalten auswählen können.";
+$l['edit_reason'] = "Grund bearbeiten";
+$l['edit_reason_desc'] = "Bearbeite einen Grund, den Benutzer beim Melden von Inhalten auswählen können.";
+
+$l['applies_to'] = "Auswählbar bei";
+$l['order'] = "Sortierung";
+$l['extra_comment'] = "Kommentar erforderlich?";
+$l['options'] = "Optionen";
+$l['delete_reason'] = "Grund löschen";
+$l['no_report_reasons'] = "Derzeit gibt es keine Melde-Gründe in deinem Forum.";
+
+$l['reason_title'] = "Title";
+$l['reason_title_desc'] = "Gib den Titel für den Melde-Grund ein.";
+$l['requires_extra'] = "Zusätzlicher Kommentar erforderlich?";
+$l['requires_extra_desc'] = "Soll ein Benutzer einen zusätzlichen Kommentar angeben müssen, wenn dieser Grund gewählt wird?";
+$l['save_reason'] = "Grund speichern";
+
+$l['content_colon'] = "Inhalt:";
+
+$l['all_content'] = "Alle Inhalte";
+$l['select_content'] = "Inhalte auswählen";
+
+// Content types
+$l['report_content_'] = "Keinen";
+$l['report_content_all'] = "Allen";
+$l['report_content_post'] = "Beiträgen";
+$l['report_content_profile'] = "Profilen";
+$l['report_content_reputation'] = "Bewertungen";
+
+// Content reasons
+$l['report_reason_rules'] = "Missachtet Foren-Regeln";
+$l['report_reason_bad'] = "Unangemessender Inhalt";
+$l['report_reason_spam'] = "Spam";
+$l['report_reason_wrong'] = "Falsches Forum";
+$l['report_reason_other'] = "Sonstiges";
+
+$l['default_report_reason'] = "Standard";
+$l['custom_report_reason'] = "Eigener";
+
+$l['update_reasons_order'] = "Sortierung aktualisieren";
+
+$l['error_cannot_modify_reason'] = "Du kannst diesen Grund nicht bearbeiten.";
+$l['error_cannot_delete_reason'] = "Du kannst diesen Grund nicht löschen.";
+$l['error_invalid_reason'] = "Du hast einen ungültigen Grund ausgewählt.";
+$l['error_missing_title'] = "Du hast keinen Titel für den Grund eingegeben.";
+//$l['error_missing_applies_to'] = "You did not select what content this reason applies to.";
+$l['error_missing_extra'] = "Du hast nicht ausgewählt, ob ein zusätzlicher Kommentar angegeben werden muss.";
+
+$l['success_reasons_disporder_updated'] = "Die Sortierung wurde erfolgreich aktualisiert.";
+$l['success_reason_created'] = "Der Grund wurde erfolgreich erstellt.";
+$l['success_reason_updated'] = "Der Grund wurde erfolgreich bearbeitet.";
+$l['success_reason_deleted'] = "Der Grund wurde erfolgreich gelöscht.";
+
+$l['confirm_reason_deletion'] = "Willst du diesen Grund wirklich löschen?";
+
