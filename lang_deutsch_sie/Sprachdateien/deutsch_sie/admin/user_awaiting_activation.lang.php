c477ec8c818c3244e05518e566f91c73604b810d
Stefan-ST <mybbcms@yahoo.de>
2016-12-21 18:10:24 +0100

    MyBB 1.8.9

---
+++ lang_deutsch_sie/Sprachdateien/deutsch_sie/admin/user_awaiting_activation.lang.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * German language pack for MyBB 1.8 (formal)
+ * Deutsches Sprachpaket für MyBB 1.8 "formell" (Sie)
+ * (c) 2005-2014 MyBBoard.de
+ * 
+ * Author/Autor: http://www.mybboard.de/
+ * License/Lizenz: GNU Lesser General Public License, Version 3
+ */
+
+$l['awaiting_activation'] = "Nicht aktivierte Benutzer";
+$l['awaiting_activation_desc'] = "Hier können Sie die Benutzer verwalten, die aktiviert werden müssen. Bitte beachten Sie, dass Benutzer, die hier aktiviert werden und deren E-Mail-Adressen noch nicht verifiziert ist, ihre E-Mail-Adresse nicht mehr verifizieren müssen.";
+
+$l['manage_awaiting_activation'] = "Nicht aktivierte Benutzer verwalten";
+
+$l['no_users_awaiting_activation'] = "Zur Zeit warten keine Benutzer auf Aktivierung.";
+
+$l['username'] = "Benutzername";
+$l['registered'] = "Registriert am";
+$l['last_active'] = "Letzte Aktivität";
+$l['email'] = "E-Mail-Adresse";
+$l['ipaddress'] = "IP-Addresse";
+$l['type'] = "Art";
+
+$l['email_activation'] = "E-Mail muss verifiziert werden";
+$l['administrator_activation'] = "Aktivierung durch Administrator";
+$l['admin_activation_coppa'] = "Aktivierung durch Administrator (COPPA)";
+
+$l['activate_users'] = "Benutzer aktivieren";
+$l['delete_users'] = "Benutzer löschen";
+
+$l['confirm_activate_users'] = "Sie Sie sicher, dass die ausgewählten Benutzer aktiviert werden sollen?";
+$l['confirm_delete_users'] = "Sind Sie sicher, dass die ausgewählten Benutzer gelöscht werden sollen?";
+
+$l['no_users_selected'] = "Sie haben keine Benutzer ausgewählt.";
+
+$l['success_users_deleted'] = "Die ausgewählten Benutzer wurden gelöscht.";
+$l['success_users_activated'] = "Die ausgewählten Benutzer wurden aktiviert.";
