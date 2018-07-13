dc17e5080328ad441863229b7234302b72518d2d
Stefan-ST <mybbcms@yahoo.de>
2018-07-13 07:41:10 +0200

    MyBB 1.8.17

--- lang_deutsch_du/Sprachdateien/deutsch_du/index.lang.php
+++ lang_deutsch_du/Sprachdateien/deutsch_du/index.lang.php
@@ -41,3 +41,4 @@ $l['online_guest_plural'] = "Gäste";
 $l['online_guest_singular'] = "Gast";
 $l['online_note'] = "{1} {2} aktiv in den letzten {3} Minuten: {4} {5}, {6} davon unsichtbar und {8} {9}";
 $l['subforums'] = "Unterforen:";
+$l['board_stats_link_separator'] = ' | ';
