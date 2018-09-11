6ac52a6d473cdefd604aad906bd04736de633276
Stefan-ST <mybbcms@yahoo.de>
2018-09-11 22:00:31 +0200

    MyBB 1.8.19

--- lang_deutsch_sie/Sprachdateien/deutsch_sie.php
+++ lang_deutsch_sie/Sprachdateien/deutsch_sie.php
@@ -18,7 +18,7 @@ $langinfo['author'] = "MyBB.de";
 $langinfo['website'] = "https://www.mybb.de/";
 
 // Compatible version of MyBB
-$langinfo['version'] = "1818";
+$langinfo['version'] = "1819";
 
 // Sets if the translation includes the Admin CP (1 = yes, 0 = no)
 $langinfo['admin'] = 1;
